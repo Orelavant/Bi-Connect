3be9720778a331edffeab705ac251f0e50876ffc
jkawamura <jkawamura@haverford.edu>
2022-04-05 19:41:05 -0400

    Added boardid folder

--- bi-connect/components/PostCommentsitem.tsx
+++ bi-connect/components/PostCommentsitem.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import CommentItem from "./CommentItem";
 
+interface PostCommentProps {
+  username: string;
+  content: string;
+  timestamp: number;
+}
 const PostCommentsitem = () => {
   return <div></div>;
 };
