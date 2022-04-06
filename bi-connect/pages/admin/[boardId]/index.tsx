c881bd664f60f5953cc3d063d44b23b03d69b066
jkawamura <jkawamura@haverford.edu>
2022-04-05 22:30:29 -0400

    Added UI library + examples

--- bi-connect/pages/admin/[boardId]/index.tsx
+++ bi-connect/pages/admin/[boardId]/index.tsx
@@ -1,82 +1,39 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { Header, Form, Comment, Button } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
+import { Item, Image } from "semantic-ui-react";
+import "fomantic-ui-css/semantic.css";
 
 const Board = ({ test }: any) => {
   const router = useRouter();
   console.log(test);
   return (
-    <Comment.Group>
-      <Header as="h3" dividing>
-        Comments
-      </Header>
+    <Item.Group>
+      <Item>
+        <Item.Image size="tiny" src="/images/wireframe/image.png" />
 
-      <Comment>
-        <Comment.Avatar src="https://react.semantic-ui.com/images/avatar/small/matt.jpg" />
-        <Comment.Content>
-          <Comment.Author as="a">Matt</Comment.Author>
-          <Comment.Metadata>
-            <div>Today at 5:42PM</div>
-          </Comment.Metadata>
-          <Comment.Text>How artistic!</Comment.Text>
-          <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
+        <Item.Content>
+          <Item.Header as="a">Header</Item.Header>
+          <Item.Meta>Description</Item.Meta>
+          <Item.Description>
+            <p>This is the content</p>
+          </Item.Description>
+          <Item.Extra>Additional Details</Item.Extra>
+        </Item.Content>
+      </Item>
 
-      <Comment>
-        <Comment.Avatar src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
-        <Comment.Content>
-          <Comment.Author as="a">Elliot Fu</Comment.Author>
-          <Comment.Metadata>
-            <div>Yesterday at 12:30AM</div>
-          </Comment.Metadata>
-          <Comment.Text>
-            <p>This has been very useful for my research. Thanks as well!</p>
-          </Comment.Text>
-          <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-        <Comment.Group>
-          <Comment>
-            <Comment.Avatar src="https://react.semantic-ui.com/images/avatar/small/jenny.jpg" />
-            <Comment.Content>
-              <Comment.Author as="a">Jenny Hess</Comment.Author>
-              <Comment.Metadata>
-                <div>Just now</div>
-              </Comment.Metadata>
-              <Comment.Text>Elliot you are always so right :)</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-        </Comment.Group>
-      </Comment>
+      <Item>
+        <Item.Image size="tiny" src="/images/wireframe/image.png" />
 
-      <Comment>
-        <Comment.Avatar src="https://react.semantic-ui.com/images/avatar/small/joe.jpg" />
-        <Comment.Content>
-          <Comment.Author as="a">Joe Henderson</Comment.Author>
-          <Comment.Metadata>
-            <div>5 days ago</div>
-          </Comment.Metadata>
-          <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-          <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
-
-      <Form reply>
-        <Form.TextArea />
-        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
-      </Form>
-    </Comment.Group>
+        <Item.Content>
+          <Item.Header as="a">Header</Item.Header>
+          <Item.Meta>Description</Item.Meta>
+          <Item.Description>
+            <p>This is more content</p>
+          </Item.Description>
+          <Item.Extra>Additional Details</Item.Extra>
+        </Item.Content>
+      </Item>
+    </Item.Group>
   );
 };
 
