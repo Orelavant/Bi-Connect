e21d5b474552e37a48dc00981d0cb6e739c769be
Jiangxue Han <jhan1@brynmawr.edu>
2022-04-10 18:03:03 -0400

    Added login page for android, setup api call using apollo client for graphql, still error for http call

--- server/build.ts
+++ server/build.ts
@@ -8,7 +8,12 @@ dotenv.config();
 export const createExpressApp = () => {
 	const app = express();
 	app.use(cookieParser());
-	app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+	app.use(
+		cors({
+			origin: ["http://localhost:3000", "http://localhost:5556"],
+			credentials: true,
+		})
+	);
 	return app;
 };
 
