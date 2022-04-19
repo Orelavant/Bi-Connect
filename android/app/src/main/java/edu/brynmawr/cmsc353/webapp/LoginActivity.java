99ceeca5b561b18f63de2ff6c6ba3fc1801a7504
ichang1 <isaac.chang717@gmail.com>
2022-04-18 20:40:13 -0400

    working on sending cookie in mobile

--- android/app/src/main/java/edu/brynmawr/cmsc353/webapp/LoginActivity.java
+++ android/app/src/main/java/edu/brynmawr/cmsc353/webapp/LoginActivity.java
@@ -1,6 +1,8 @@
 package edu.brynmawr.cmsc353.webapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,10 +19,14 @@ import com.apollographql.apollo.api.Error;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import com.apollographql.apollo.exception.ApolloHttpException;
+import com.apollographql.apollo.request.RequestHeaders;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -57,9 +63,51 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String email, String password) {
         Log.i(TAG, "Attempting to login user " + email);
+
+        SharedPreferences preferences = getSharedPreferences("BI_CONNECT_STORAGE", Context.MODE_PRIVATE);
+        String cookie = preferences.getString("biConnectAccessToken", null);
+
+        RequestHeaders requestHeaders;
+        Map<String, String> headers = new HashMap<>();
+        if (cookie != null) {
+            headers.put("biConnectAccessToken", cookie);
+
+        }
+        requestHeaders = RequestHeaders.builder().addHeaders(headers).build();
+
         ApolloClient apolloClient = ApolloClient.builder()
                 .serverUrl("http://10.0.2.2:3001/graphql")
                 .build();
+
+        apolloClient.query(new IsLoggedInQuery())
+                .enqueue(new ApolloCall.Callback<IsLoggedInQuery.Data>() {
+                    @Override
+                    public void onResponse(@NonNull Response<IsLoggedInQuery.Data> response) {
+                        List<Error> errors = response.getErrors();
+                        if (errors == null || (errors != null && errors.size() > 0)) {
+                            return;
+                        }
+                        // cookie with good token so logged in
+                        String username = response.getData().isLoggedIn().username;
+                        String email = response.getData().isLoggedIn().username;
+
+                        SharedPreferences preferences = getSharedPreferences("BI_CONNECT_STORAGE", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("username", username);
+                        editor.putString("email", email);
+                        editor.apply();
+
+                        // redirect to main
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
+                  }
+
+                  @Override
+                  public void onFailure(@NonNull ApolloException e) {
+                        Log.i(TAG, "Login failed for: " + email);
+                  }
+              }
+        );
 
 
         apolloClient.mutate(new LoginMutation(new LoginInput(email, password)))
@@ -78,8 +126,15 @@ public class LoginActivity extends AppCompatActivity {
                     });
                     return;
                 }
+                String cookie = response.getData().login();
+                // set cookie to preferences
+                SharedPreferences preferences = getSharedPreferences("BI_CONNECT_STORAGE", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("biConnectAccessToken", cookie);
+                editor.apply();
 
                 Log.i(TAG, "Login success for :" + email);
+                Log.v(TAG, response.toString());
                 goMainActivity();
                 runOnUiThread(new Runnable() {
 
