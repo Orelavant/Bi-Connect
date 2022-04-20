55f82102c587e391b5b082605c79aa395cab15b5
jkawamura <jkawamura@haverford.edu>
2022-04-20 15:15:28 -0400

    Added queries

---
+++ android/app/src/main/java/edu/brynmawr/cmsc353/webapp/CommentActivity.java
@@ -0,0 +1,15 @@
+package edu.brynmawr.cmsc353.webapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+    }
+}
